64f206d02680c6dde56a40f5f8dfab27e4e44cea
Joshua Giudici <jtgiudici@gmail.com>
2015-04-11 21:58:12 -0400

    configured api key in themoviedb gem

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module MovieApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+		Tmdb::Api.key(ENV["THEMOVIEDB_API_KEY"])
   end
 end
